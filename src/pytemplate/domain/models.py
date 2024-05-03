26da677816f7af6b10707b1543394e40c169d0c7
Rafael <thesaintrapha@gmail.com>
2024-05-03 23:38:48 +0400

    test: Tested operands_factory factory method

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -5,3 +5,11 @@ from dataclasses import dataclass
 class Operands:
     first_operand: int
     second_operand: int
+
+    def __post_init__(self):
+        if not isinstance(self.first_operand, int) or not isinstance(self.second_operand, int):
+            raise TypeError("Both operands must be integers")
+
+
+def operand_factory(first_operand: int, second_operand: int) -> Operands:
+    return Operands(first_operand=first_operand, second_operand=second_operand)
