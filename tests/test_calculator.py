0e410e726015b898b695e80195c97d1ff665af80
Rafael <thesaintrapha@gmail.com>
2024-05-04 13:48:57 +0400

    test: Renamed test names

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,27 +2,27 @@ from src.pytemplate.domain.models import operands_factory
 from src.pytemplate.service.calculator import Calculator
 
 
-def test_addition():
+def test_add():
     assert Calculator().add(operands_factory(10, 20)) == 30
 
 
-def test_subtraction():
+def test_subtract():
     assert Calculator().subtract(operands_factory(10, 20)) == -10
 
 
-def test_multiplication():
+def test_multiply():
     assert Calculator().multiply(operands_factory(10, 20)) == 200
 
 
-def test_division():
+def test_divide():
     assert Calculator().divide(operands_factory(20, 10)) == 2
 
 
-def test_division_should_return_zero_when_divisor_is_greater_than_dividend():
+def test_divide_with_small_divisor():
     assert Calculator().divide(operands_factory(10, 20)) == 0
 
 
-def test_division_should_return_correct_floor_integer_when_division_is_not_exact():
+def test_divide_with_not_exact_division():
 
     result = Calculator().divide(operands_factory(10, 3))
 
