8dd3cdb2d407a10ac8d5c9641ccc35d7fd761414
Rafael <thesaintrapha@gmail.com>
2024-05-05 14:23:39 +0400

    feat: Added main function for cli operations

---
+++ src/pytemplate/entrypoints/cli/main.py
@@ -0,0 +1,24 @@
+from pytemplate.domain.models import operands_factory
+from pytemplate.service.calculator import Calculator
+
+
+def main():
+
+    available_actions = ("add", "subtract", "multiply", "divide")
+
+    first_operand = int(input("Enter first integer number: "))
+    second_operand = int(input("Enter second integer number: "))
+    action = input(f"Enter one action {available_actions}: ")
+    calculator = Calculator()
+
+    if action in available_actions:
+        operands = operands_factory(first_operand, second_operand)
+        result = calculator.__getattribute__(action)(operands)
+        print(result)
+
+    else:
+        print("Invalid action name")
+
+
+if __name__ == "__main__":
+    main()
