f48c2e6db0bb373ab5ec742c7b0f5574bb02d8e9
Rafael <thesaintrapha@gmail.com>
2024-05-06 11:40:18 +0400

    test: Rewrote tests with unittest.mock

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -1,57 +1,22 @@
+from unittest.mock import patch
+
 import pytest
 
 from pytemplate.entrypoints.cli.main import main
 
 
-@pytest.fixture
-def mock_inputs(monkeypatch):
-    def _mock_inputs(inputs):
-        input_generator = (i for i in inputs)
-        monkeypatch.setattr("builtins.input", lambda _: next(input_generator))
-
-    return _mock_inputs
-
-
-def test_addition(mock_inputs, capsys):
-    inputs = ["2", "3", "add"]
-    expected = "5"
-    mock_inputs(inputs)
-    main()
-    captured = capsys.readouterr()
-    assert captured.out == expected
-
-
-def test_subtraction(mock_inputs, capsys):
-    inputs = ["5", "3", "subtract"]
-    expected = "2"
-    mock_inputs(inputs)
-    main()
-    captured = capsys.readouterr()
-    assert captured.out == expected
-
-
-def test_multiplication(mock_inputs, capsys):
-    inputs = ["4", "2", "multiply"]
-    expected = "8"
-    mock_inputs(inputs)
-    main()
-    captured = capsys.readouterr()
-    assert captured.out == expected
-
-
-def test_division(mock_inputs, capsys):
-    inputs = ["8", "4", "divide"]
-    expected = "2"
-    mock_inputs(inputs)
-    main()
-    captured = capsys.readouterr()
-    assert captured.out == expected
-
-
-def test_invalid_action(mock_inputs, capsys):
-    inputs = ["8", "4", "invide_action_name"]
-    expected = "Invalid action name"
-    mock_inputs(inputs)
-    main()
-    captured = capsys.readouterr()
-    assert captured.out == expected
+@pytest.mark.parametrize(
+    "inputs, expected_output",
+    [
+        (["10", "11", "add"], "21"),
+        (["21", "11", "subtract"], "10"),
+        (["4", "4", "multiply"], "16"),
+        (["20", "5", "divide"], "4"),
+        (["15", "3", "invalid"], "Invalid action name"),
+    ],
+)
+def test_main(capsys, inputs, expected_output):
+    with patch("builtins.input", side_effect=inputs):
+        main()
+        captured = capsys.readouterr()
+        assert expected_output in captured.out
