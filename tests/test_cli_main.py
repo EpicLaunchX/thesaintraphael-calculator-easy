a051f92acec2b89498217e228a1a03bd612fb782
Rafael <thesaintrapha@gmail.com>
2024-05-07 10:18:27 +0400

    test: Tested using mock.patch

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -1,35 +1,34 @@
-from unittest.mock import patch
-
+from unittest import mock
 from pytemplate.entrypoints.cli.main import main
 
 
 class TestCalcFunctions:
     def test_add_action(self, capsys):
-        with patch("builtins.input", side_effect=["45", "35", "add"]):
+        with mock.patch("builtins.input", side_effect=["45", "35", "add"]):
             main()
             captured = capsys.readouterr()
             assert "80" in captured.out
 
     def test_subtract_action(self, capsys):
-        with patch("builtins.input", side_effect=["45", "35", "subtract"]):
+        with mock.patch("builtins.input", side_effect=["45", "35", "subtract"]):
             main()
             captured = capsys.readouterr()
             assert "10" in captured.out
 
     def test_multiply_action(self, capsys):
-        with patch("builtins.input", side_effect=["4", "5", "multiply"]):
+        with mock.patch("builtins.input", side_effect=["4", "5", "multiply"]):
             main()
             captured = capsys.readouterr()
             assert "20" in captured.out
 
     def test_divide_action(self, capsys):
-        with patch("builtins.input", side_effect=["20", "5", "divide"]):
+        with mock.patch("builtins.input", side_effect=["20", "5", "divide"]):
             main()
             captured = capsys.readouterr()
             assert "4" in captured.out
 
     def test_invalid_action(self, capsys):
-        with patch("builtins.input", side_effect=["20", "5", "invalid"]):
+        with mock.patch("builtins.input", side_effect=["20", "5", "invalid"]):
             main()
             captured = capsys.readouterr()
             assert "Invalid action name" in captured.out
