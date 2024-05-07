ef07e076944e73143f609da65673e68d4abeed40
Rafael <thesaintrapha@gmail.com>
2024-05-07 10:06:42 +0400

    test: Rewrote tests with unittest.mock

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -1,22 +1,34 @@
 from unittest.mock import patch
 
-import pytest
-
 from pytemplate.entrypoints.cli.main import main
 
 
-@pytest.mark.parametrize(
-    "inputs, expected_output",
-    [
-        (["10", "11", "add"], "21"),
-        (["21", "11", "subtract"], "10"),
-        (["4", "4", "multiply"], "16"),
-        (["20", "5", "divide"], "4"),
-        (["15", "3", "invalid"], "Invalid action name"),
-    ],
-)
-def test_main(capsys, inputs, expected_output):
-    with patch("builtins.input", side_effect=inputs):
+def test_add_action(capsys):
+    with patch("builtins.input", side_effect=["10", "20", "add"]):
+        main()
+        assert "30" == capsys.readouterr().out
+
+
+def test_subtract_action(capsys):
+    with patch("builtins.input", side_effect=["20", "10", "subtract"]):
+        main()
+        assert "10" == capsys.readouterr().out
+
+
+def test_multiply_action(capsys):
+    with patch("builtins.input", side_effect=["10", "2", "multiply"]):
+        main()
+        assert "20" == capsys.readouterr().out
+
+
+def test_divide_action(capsys):
+    with patch("builtins.input", side_effect=["10", "5", "divide"]):
         main()
         captured = capsys.readouterr()
-        assert expected_output in captured.out
+        assert "2" in captured.out
+
+
+def test_invalid_action(capsys):
+    with patch("builtins.input", side_effect=["4", "5", "invalid"]):
+        main()
+        assert "Invalid action name" in capsys.readouterr().out
