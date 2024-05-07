7c785acf6684199a79a41007af095aaf31ffb751
Rafael <thesaintrapha@gmail.com>
2024-05-07 10:08:41 +0400

    test: Rewrote test with Unittest class suite

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -3,32 +3,33 @@ from unittest.mock import patch
 from pytemplate.entrypoints.cli.main import main
 
 
-def test_add_action(capsys):
-    with patch("builtins.input", side_effect=["10", "20", "add"]):
-        main()
-        assert "30" == capsys.readouterr().out
+class TestCalcFunctions:
+    def test_add_action(self, capsys):
+        with patch("builtins.input", side_effect=["45", "35", "add"]):
+            main()
+            captured = capsys.readouterr()
+            assert "80" in captured.out
 
+    def test_subtract_action(self, capsys):
+        with patch("builtins.input", side_effect=["45", "35", "subtract"]):
+            main()
+            captured = capsys.readouterr()
+            assert "10" in captured.out
 
-def test_subtract_action(capsys):
-    with patch("builtins.input", side_effect=["20", "10", "subtract"]):
-        main()
-        assert "10" == capsys.readouterr().out
+    def test_multiply_action(self, capsys):
+        with patch("builtins.input", side_effect=["4", "5", "multiply"]):
+            main()
+            captured = capsys.readouterr()
+            assert "20" in captured.out
 
+    def test_divide_action(self, capsys):
+        with patch("builtins.input", side_effect=["20", "5", "divide"]):
+            main()
+            captured = capsys.readouterr()
+            assert "4" in captured.out
 
-def test_multiply_action(capsys):
-    with patch("builtins.input", side_effect=["10", "2", "multiply"]):
-        main()
-        assert "20" == capsys.readouterr().out
-
-
-def test_divide_action(capsys):
-    with patch("builtins.input", side_effect=["10", "5", "divide"]):
-        main()
-        captured = capsys.readouterr()
-        assert "2" in captured.out
-
-
-def test_invalid_action(capsys):
-    with patch("builtins.input", side_effect=["4", "5", "invalid"]):
-        main()
-        assert "Invalid action name" in capsys.readouterr().out
+    def test_invalid_action(self, capsys):
+        with patch("builtins.input", side_effect=["20", "5", "invalid"]):
+            main()
+            captured = capsys.readouterr()
+            assert "Invalid action name" in captured.out
